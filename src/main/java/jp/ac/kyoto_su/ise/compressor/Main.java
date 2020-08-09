df93c06684b9c87410e615b26399754a306f629d
OKA <g1644222@cc.kyoto-su.ac.jp>
2020-08-09 12:01:05 +0900

    変更したファイルに出し忘れがあっため、追加でupする。(javadocのコメント付きファイル、verの数を増やしたMainファイル)

--- src/main/java/jp/ac/kyoto_su/ise/compressor/Main.java
+++ src/main/java/jp/ac/kyoto_su/ise/compressor/Main.java
@@ -7,9 +7,16 @@ import java.nio.file.Paths;
 import java.util.Optional;
 
 public class Main {
-    public static final String VERSION = "1.0.0";
+  /**
+  *圧縮をするためのメインプログラム。
+  *ここでは引数を解析するクラスやデータを圧縮するクラスにデータを渡す。
+  */
+    public static final String VERSION = "1.2.0";
 
     public Main(String[] arguments) throws Exception {
+      /**
+      *引数を解析してヘルプとバージョンの情報を出力するか、圧縮処理をするかを決定する。
+      */
         Arguments args = Arguments.parse(arguments);
         if(args.versioninformation()) {
           System.out.println(Arguments.version());
@@ -24,6 +31,9 @@ public class Main {
     }
 
     public void perform(Arguments args) throws NoCompressorException {
+      /**
+      *引数のファイルをストリームにして圧縮するメソッドに渡す。
+      */
         Compressor compressor = args.compressor();
         args.stream().map(file -> Pair.of(file, compressor.renameFile(file)))  //mapに指定された形式にしたストリームを返す
                 .forEach(pair -> performEach(compressor, pair));
@@ -34,6 +44,9 @@ public class Main {
     }
 
     public void performEach(Compressor compressor, Pair<String, String> pair) {
+      /**
+      *ストリーム情報に合わせて圧縮する方式を帰るメソッド。
+      */
         String originalFileName = pair.map((l, r) -> l);
         String destFileName = pair.map((l, r) -> r);
 
--- src/main/java/jp/ac/kyoto_su/ise/compressor/NoCompressorException.java
+++ src/main/java/jp/ac/kyoto_su/ise/compressor/NoCompressorException.java
@@ -1,6 +1,9 @@
 package jp.ac.kyoto_su.ise.compressor;
 
 public class NoCompressorException extends Exception {
+  /**
+  * 圧縮するクラスの呼び出しに失敗した場合、エラー原因を出力する
+  */
     public NoCompressorException(String message) {
         super(message);
     }
